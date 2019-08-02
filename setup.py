127c3861887e89f6c6a74b75c0cfba72b0a6963c
Paul Mikesell <pmikesell@gmail.com>
2019-08-02 09:30:56 -0700

    More cleanup, trying to make this simpler to externally use.

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="lsm9ds1",
     version="0.9.2",
-    author="Richard Gemmell",
-    author_email="16683467+Richard-Gemmell@users.noreply.github.com",
     description="A python module to enable a Raspberry Pi to access an LSM9DS1 IMU sensor.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Richard-Gemmell/lsm9ds1-rjg",
     license="LICENSE.txt",
     packages=setuptools.find_packages(),
     classifiers=[
